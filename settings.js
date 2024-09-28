e7bb538b3418572f198d88d10c18cd3fa0fc1274
RaniduWishwajith <110620296+RaniduWishwajith@users.noreply.github.com>
2024-09-28 18:49:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,39 +2,46 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==wY38UcBZHVSh0bIlzczdHaxgTTppVaX9mUUtGdjREcsBjSlRkbP12NHlVMjIVeUZkUxYFapwd=14566192749',
   ALIVE_REACT: '👋',
-  ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
+  ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ]
+I Ａｍ ＡＬＩＶＥ Ｎｏw !
+
+𝗧𝗬𝗣𝗘 .menu to command lis𝘁
+
+Bot Name : RV- MD
+Owner : 𝗥𝗔𝗡𝗜𝗗𝗨 𝗪𝗜𝗦𝗛𝗪𝗔𝗝𝗜𝗧𝗛
 
 ⏰ Now time is @time
 📅 Date : @date
 
 🟢 I am online still @uptime
 🔄 Response Delay : @ping Ms
-💻 Host : @host
 🪢 Version : @version
 
 ☫ Work Mode : @mode
 ☫ Work Type : @worktype
 
-Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
-  ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
+*© Ｒ Ｖ - ＭＤ  |  ２０２４*
+
+RV- MD ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
+  ALIVE_MEDIA: 'https://i.ibb.co/MsFF8JQ/IMG-20240420-WA0004-1.jpg',
   MONGODB_URL: '', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
-  BOT_NAME: '𝐐𝐔𝐄𝐄𝐍 𝐄𝐋𝐈𝐒𝐀',
-  OWNER_NAME: 'ᴍ ʀ  ɴ ɪ ᴍ ᴀ',
-  OWNER_NUMBER: '94715166712',
-  SUDO: '94715166712',
+  BOT_NAME: '𝚁𝚅-𝙼𝙳',
+  OWNER_NAME: '𝚁𝙰𝙽𝙸𝙳𝚄 𝚆𝙸𝚂𝙷𝚆𝙰𝙹𝙸𝚃𝙷',
+  OWNER_NUMBER: '94773077959',
+  SUDO: '94773077959',
   WORK_TYPE: 'public',
   WORK_MODE: 'group',
   TIME_ZONE: 'Asia/Colombo',
-  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴍᴅ',
-  STICKER_AUTHOR: 'Qᴜᴇᴇɴ ᴇʟɪꜱᴀ',
-  STICKER_PACK: 'ᴍʀ ɴɪᴍᴀ ᴏꜰᴄ',
+  CAPTION: '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ʀᴠ-ᴍᴅ',
+  STICKER_AUTHOR: 'ʀᴠ-ᴍᴅ',
+  STICKER_PACK: 'ᴍʀ ʀᴠ-ᴍᴅ ᴏꜰᴄ',
   REMOVE_BG_APIKEY: '',
-  MAXIMUM_FILESIZE: 90,
+  MAXIMUM_FILESIZE: 300,
   OWNER_R_EMOJI: '👽',
   ALIVE_AS_GIF: true,
   AUTO_VOICE: false,
@@ -57,7 +64,7 @@ Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ANTI_BOT: false,
   BUTTON_MESSAGES: true,
   SONG_DOWNLOAD_TYPE: 'selectable', // audio , document
-  VIDEO_DOWNLOAD_TYPE: 'selectable', // video
+  VIDEO_DOWNLOAD_TYPE: 'selectable', // video , document
   FB_DOWNLOAD_TYPE: 'selectable', // hdvideo , sdvideo
   TIKTOK_DOWNLOAD_TYPE: 'selectable', // nowmvideo , wmvideo
   MSG: {
